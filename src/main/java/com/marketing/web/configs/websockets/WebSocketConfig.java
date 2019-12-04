8437df178da9e3f02c79e7f319617a87284d3f7a
Yusuf Can Celik <yusufcancelik@hotmail.com>
2019-12-04 23:33:51 +0300

    bug fix

--- src/main/java/com/marketing/web/configs/websockets/WebSocketConfig.java
+++ src/main/java/com/marketing/web/configs/websockets/WebSocketConfig.java
@@ -4,7 +4,6 @@ import com.marketing.web.enums.WebSocketType;
 import com.marketing.web.services.websocket.WebSocketNotificationSenderService;
 import com.marketing.web.services.websocket.WebSocketProductSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -29,20 +28,20 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry
                                                   webSocketHandlerRegistry) {
 
-        webSocketHandlerRegistry.addHandler(createHandler(),
+        webSocketHandlerRegistry.addHandler(createHandler(WebSocketType.NOTIFY),
                 "/user/queue/notifications")
-                .addHandler("/queue/products")
+                .addHandler(createHandler(WebSocketType.PRDCT),"/queue/products")
                 .setHandshakeHandler(new AuthenticationHandshakeHandler(webSocketJWTValidator, "setPrincipal"));
     }
 
-    @Bean
     public WebSocketHandler createHandler(WebSocketType type) {
         switch (type) {
             case NOTIFY:
                 return new NotificationWebSocketHandler(notificationSenderService);
-                break;
             case PRDCT:
                 return new ProductsWebSocketHandler(productSenderService);
+            default:
+                return null;
         }
     }
 }
