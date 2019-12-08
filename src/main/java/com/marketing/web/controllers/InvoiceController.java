3f76b3c97f3632ce75a76434fb21b4dcca8e08a3
Yusuf Can Celik <yusufcancelik@hotmail.com>
2019-12-09 00:02:31 +0300

    refactor

--- src/main/java/com/marketing/web/controllers/InvoiceController.java
+++ src/main/java/com/marketing/web/controllers/InvoiceController.java
@@ -2,11 +2,14 @@ package com.marketing.web.controllers;
 
 import com.marketing.web.dtos.WrapperPagination;
 import com.marketing.web.dtos.invoice.ReadableInvoice;
+import com.marketing.web.enums.RoleType;
+import com.marketing.web.errors.BadRequestException;
 import com.marketing.web.models.Invoice;
 import com.marketing.web.models.User;
 import com.marketing.web.services.invoice.InvoiceService;
 import com.marketing.web.services.user.UserService;
 import com.marketing.web.utils.mappers.InvoiceMapper;
+import com.marketing.web.utils.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,27 +25,34 @@ public class InvoiceController {
     private InvoiceService invoiceService;
 
     @GetMapping
-    public ResponseEntity<WrapperPagination<ReadableInvoice>> getAll(@RequestParam(required = false) Integer pageNumber){
-        if (pageNumber == null){
-            pageNumber=1;
+    public ResponseEntity<WrapperPagination<ReadableInvoice>> getAll(@RequestParam(required = false) String userId, @RequestParam(required = false) Integer pageNumber) {
+        if (pageNumber == null) {
+            pageNumber = 1;
         }
         User user = userService.getLoggedInUser();
-
+        if (UserMapper.roleToRoleType(user.getRole()).equals(RoleType.ADMIN)) {
+            if (!userId.isEmpty()) {
+                User userByUserId = userService.findByUUID(userId);
+                return ResponseEntity.ok(
+                        InvoiceMapper.pagedInvoiceListToWrapperReadableInvoice(invoiceService.findAllByUser(userByUserId, pageNumber)));
+            }
+            return ResponseEntity.ok(InvoiceMapper.pagedInvoiceListToWrapperReadableInvoice(invoiceService.findAll(pageNumber)));
+        }
         return ResponseEntity.ok(
                 InvoiceMapper.pagedInvoiceListToWrapperReadableInvoice(invoiceService.findAllByUser(user, pageNumber)));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ReadableInvoice> getInvoiceById(@PathVariable String id){
+    public ResponseEntity<ReadableInvoice> getInvoiceById(@PathVariable String id) {
         User user = userService.getLoggedInUser();
         Invoice invoice = invoiceService.findByUUIDAndUser(id, user);
         return ResponseEntity.ok(InvoiceMapper.invoiceToReadableInvoice(invoice));
     }
 
     @PostMapping("/byOrder/{orderId}")
-    public ResponseEntity<ReadableInvoice> getByOrder(@PathVariable String orderId){
+    public ResponseEntity<ReadableInvoice> getByOrder(@PathVariable String orderId) {
         User user = userService.getLoggedInUser();
-        Invoice invoice = invoiceService.findByOrderAndUser(orderId,user);
+        Invoice invoice = invoiceService.findByOrderAndUser(orderId, user);
         return ResponseEntity.ok(InvoiceMapper.invoiceToReadableInvoice(invoice));
     }
 
--- src/main/java/com/marketing/web/controllers/OrderController.java
+++ src/main/java/com/marketing/web/controllers/OrderController.java
@@ -3,6 +3,7 @@ package com.marketing.web.controllers;
 import com.marketing.web.dtos.WrapperPagination;
 import com.marketing.web.dtos.order.*;
 import com.marketing.web.enums.RoleType;
+import com.marketing.web.errors.BadRequestException;
 import com.marketing.web.errors.ResourceNotFoundException;
 import com.marketing.web.models.Order;
 import com.marketing.web.models.User;
@@ -42,13 +43,20 @@ public class OrderController {
     private Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @GetMapping
-    public ResponseEntity<WrapperPagination<ReadableOrder>> getOrders(@RequestParam(required = false) Integer pageNumber) {
+    public ResponseEntity<WrapperPagination<ReadableOrder>> getOrders(@RequestParam(required = false) String userId, @RequestParam(required = false) Integer pageNumber) {
         if (pageNumber == null) {
             pageNumber = 1;
         }
         User user = userService.getLoggedInUser();
 
         if (UserMapper.roleToRoleType(user.getRole()).equals(RoleType.ADMIN)) {
+            if (!userId.isEmpty()) {
+                User userByUserId = userService.findByUUID(userId);
+                if (!UserMapper.roleToRoleType(userByUserId.getRole()).equals(RoleType.MERCHANT)) {
+                    throw new BadRequestException("User does not have merchant role");
+                }
+                return ResponseEntity.ok(OrderMapper.pagedOrderListToWrapperReadableOrder(orderService.findAllByUser(userByUserId, pageNumber)));
+            }
             return ResponseEntity.ok(OrderMapper.pagedOrderListToWrapperReadableOrder(orderService.findAll(pageNumber)));
         }
 
@@ -57,15 +65,17 @@ public class OrderController {
 
     @PreAuthorize("hasRole('ROLE_MERCHANT')")
     @GetMapping("/summary")
-    public ResponseEntity<OrderSummary> getOrderSummary(){
+    public ResponseEntity<OrderSummary> getOrderSummary() {
         User user = userService.getLoggedInUser();
         return ResponseEntity.ok(orderService.groupBy(user));
     }
+
     @GetMapping("/summary/byUser/{userId}")
-    public ResponseEntity<OrderSummary> getUserOrderSummary(@PathVariable String userId){
+    public ResponseEntity<OrderSummary> getUserOrderSummary(@PathVariable String userId) {
         User user = userService.findByUUID(userId);
         return ResponseEntity.ok(orderService.groupBy(user));
     }
+
     @PostMapping("/filter")
     public ResponseEntity<List<ReadableOrder>> getOrdersByFilter(@RequestBody SearchOrder searchOrder) {
         User user = userService.getLoggedInUser();
