8888a9cafa5b527c88fe021bee3b45fd14f91933
Yusuf Can Celik <yusufcancelik@hotmail.com>
2019-12-15 06:32:11 +0300

    removed unused endpoints added get all user endpoint

--- src/main/java/com/marketing/web/controllers/UsersController.java
+++ src/main/java/com/marketing/web/controllers/UsersController.java
@@ -42,15 +42,12 @@ public class UsersController {
     @Autowired
     private StateService stateService;
 
-    @Autowired
-    private OrderService orderService;
-
-    @Autowired
-    private ObligationService obligationService;
-
-    @Autowired
-    private ProductSpecifyService productSpecifyService;
     private Logger logger = LoggerFactory.getLogger(UsersController.class);
+
+    @GetMapping
+    public ResponseEntity<List<ReadableUserInfo>> getAllUser() {
+        return ResponseEntity.ok(userService.findAll().stream().map(UserMapper::userToReadableUserInfo).collect(Collectors.toList()));
+    }
 
     @GetMapping("/{roleType}")
     public ResponseEntity<?> getUsersByRole(@PathVariable String roleType, @RequestParam(required = false) Boolean status) {
@@ -93,89 +90,6 @@ public class UsersController {
             default:
                 return ResponseEntity.ok(UserMapper.userToAdmin(user));
         }
-    }
-
-    // TODO Kaldirilacak
-    @PostMapping("/setActive/{id}")
-    public ResponseEntity<?> setActiveUser(@PathVariable String id) {
-        User user = userService.findByUUID(id);
-        user.setStatus(true);
-        user = userService.update(user.getId(), user);
-        RoleType role = RoleType.valueOf(user.getRole().getName().split("_")[1].toUpperCase());
-        switch (role) {
-            case CUSTOMER:
-                return ResponseEntity.ok(UserMapper.userToCustomer(user));
-            case MERCHANT:
-                return ResponseEntity.ok(UserMapper.userToMerchant(user));
-            default:
-                return ResponseEntity.ok(UserMapper.userToAdmin(user));
-        }
-    }
-
-    // TODO Kaldirilacak
-    @PostMapping("/setPassive/{id}")
-    public ResponseEntity<?> setPassiveUser(@PathVariable String id) {
-        User user = userService.findByUUID(id);
-        user.setStatus(false);
-        user = userService.update(user.getId(), user);
-        RoleType role = RoleType.valueOf(user.getRole().getName().split("_")[1].toUpperCase());
-        switch (role) {
-            case CUSTOMER:
-                return ResponseEntity.ok(UserMapper.userToCustomer(user));
-            case MERCHANT:
-                return ResponseEntity.ok(UserMapper.userToMerchant(user));
-            default:
-                return ResponseEntity.ok(UserMapper.userToAdmin(user));
-        }
-    }
-
-    // TODO Kaldirilacak
-    @GetMapping("/customers")
-    public ResponseEntity<List<CustomerUser>> getAllCustomers() {
-
-        return ResponseEntity.ok(userService.findAllByRole(RoleType.CUSTOMER).stream()
-                .map(UserMapper::userToCustomer)
-                .collect(Collectors.toList()));
-    }
-
-    // TODO Kaldirilacak
-    @GetMapping("/merchant")
-    public ResponseEntity<List<MerchantUser>> getAllMerchants() {
-        return ResponseEntity.ok(userService.findAllByRole(RoleType.MERCHANT).stream()
-                .map(UserMapper::userToMerchant)
-                .collect(Collectors.toList()));
-    }
-
-    // TODO Kaldirilacak
-    @GetMapping("/merchant/passive")
-    public ResponseEntity<List<MerchantUser>> getPassiveMerchantUsers() {
-        return ResponseEntity.ok(userService.findAllByRoleAndStatus(RoleType.MERCHANT, false).stream()
-                .map(UserMapper::userToMerchant)
-                .collect(Collectors.toList()));
-    }
-
-    // TODO Kaldirilacak
-    @GetMapping("/merchant/active")
-    public ResponseEntity<List<MerchantUser>> getActiveMerchantUsers() {
-        return ResponseEntity.ok(userService.findAllByRoleAndStatus(RoleType.MERCHANT, true).stream()
-                .map(UserMapper::userToMerchant)
-                .collect(Collectors.toList()));
-    }
-
-    // TODO Kaldirilacak
-    @GetMapping("/customers/passive")
-    public ResponseEntity<List<CustomerUser>> getPassiveCustomerUsers() {
-        return ResponseEntity.ok(userService.findAllByRoleAndStatus(RoleType.CUSTOMER, false).stream()
-                .map(UserMapper::userToCustomer)
-                .collect(Collectors.toList()));
-    }
-
-    // TODO Kaldirilacak
-    @GetMapping("/customers/active")
-    public ResponseEntity<List<CustomerUser>> getActiveCustomerUsers() {
-        return ResponseEntity.ok(userService.findAllByRoleAndStatus(RoleType.CUSTOMER, true).stream()
-                .map(UserMapper::userToCustomer)
-                .collect(Collectors.toList()));
     }
 
     @PostMapping
