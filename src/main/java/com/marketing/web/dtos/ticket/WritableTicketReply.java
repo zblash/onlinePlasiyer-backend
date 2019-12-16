f9d40745ff1b8330d0aafdd45fd595afc3b7ebe5
Yusuf Can Celik <yusufcancelik@hotmail.com>
2019-12-16 07:05:48 +0300

    refactor for constraints

--- src/main/java/com/marketing/web/dtos/ticket/WritableTicketReply.java
+++ src/main/java/com/marketing/web/dtos/ticket/WritableTicketReply.java
@@ -2,11 +2,9 @@ package com.marketing.web.dtos.ticket;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data
